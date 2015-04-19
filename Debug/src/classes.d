bc9454cee4d0de40046abb2b2a51e530bd206c9a
xanter <romanov6416@yandex.ru>
2015-04-20 01:48:22 +0300

    final version

--- Debug/src/classes.d
+++ Debug/src/classes.d
@@ -1,7 +1,4 @@
 src/classes.d: ../src/classes.cpp \
- /home/andrey/works/dyn_sheduling/include/classes.h \
- /home/andrey/works/dyn_sheduling/tinyxml2/tinyxml2.h
+ /home/andrey/works/dyn_sheduling/include/classes.h
 
 /home/andrey/works/dyn_sheduling/include/classes.h:
-
-/home/andrey/works/dyn_sheduling/tinyxml2/tinyxml2.h:
